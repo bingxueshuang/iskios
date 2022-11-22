2293ae3f52adb4808c65be42fe225e9b549b00e7
bingxueshuang <manojna.gopher@gmail.com>
2022-11-22 22:49:33 +0530

    chore(switch): setup tooling for `switch`(#8)
    * chore(tools): setup linter
    
    configure eslint for svelte (adopt google style guide)
    
    * chore(tools): setup formatter
    
    configure prettier for code formatting
    
    * chore(releases): setup automated releases for `switch`
    
    configure package to be released through github actions

---
+++ packages/switch/.eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+  	"eslint:recommended",
+  	"google",
+  	"prettier"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  env: {
+    es2020: true,
+    browser: true
+  },
+  plugins: [
+    'svelte3',
+    '@typescript-eslint'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  rules: {},
+  settings: {
+  	'svelte3/typescript': true
+  }
+};
